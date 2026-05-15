54a259ebe881f66838bb28f32e4a44c384c52356
annaluizacarvalhaes-star <annaluizacarvalhaes@gmail.com>
2026-05-15 11:06:50 -0300

    Adicionei as infos do menu

--- cp3.c
+++ cp3.c
@@ -1,13 +1,29 @@
 #include <stdio.h>
+ int opcao;
 int main(){
-    int opcao;
     do{
         system("clm");
         exibirMenu();
         scanf("%d", &opcao);
     } while (opcao != 0);
     printf("Obrigada por usar o nosso ATM!");
+}
+void exibirMenu(){
+    printf("\n");
+    printf("CAIXA ELETRONICO");
+    printf("\n");
+    printf("1 - Consultar Saldo \n");
+    printf("2 - Realizar Saque \n");
+    printf("3 - Realizar deposito \n");
+    printf(" 0 - Sair");
+    printf("================================\n");
+    printf(" Escolha uma opcao: ");
 
+    scanf("%d", &opcao);
+    while (getchar() != '\n');
+    if (opcao < 0 || opcao > 3){
+        printf("Opcao invalida");
+    }
 
 
 }
